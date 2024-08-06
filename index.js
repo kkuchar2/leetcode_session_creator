95b238f38b40b339dc361bedd3aed9b05d7d7697
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2024-08-06 10:38:11 +0200

    Add option to retry in case when requests fails

--- index.js
+++ index.js
@@ -2,15 +2,37 @@ const LEETCODE_SESSION = "MY_LEETCODE_SESSION"
 const CSRF_TOKEN = "MY_CSRF_TOKEN"
 const NEW_SESSION_NAME = "MyNewSession1";
 
-fetch("https://leetcode.com/session/", {
-    "headers": {
+const fetchWithRetry = async (url, options, retries) => {
+    for (let i = 0; i < retries; i++) {
+        try {
+            const response = await fetch(url, options);
+            if (response.ok) {
+                console.log('Request succeeded:', response);
+                return response;
+            } else {
+                console.log('Request failed');
+            }
+        } catch (error) {
+            console.log('Fetch error:', error);
+        }
+    }
+    console.log(`All ${retries} attempts failed.`);
+};
+
+const url = "https://leetcode.com/session/";
+const options = {
+    headers: {
         "content-type": "application/json",
         "x-csrftoken": CSRF_TOKEN,
         "x-requested-with": "XMLHttpRequest",
         "cookie": `LEETCODE_SESSION=${LEETCODE_SESSION};`
     },
-    "body": JSON.stringify({
-        "func": "create",
-        "name": NEW_SESSION_NAME
-    }), "method": "PUT"
-}).then(response => console.log(response));
+    body: JSON.stringify({
+        func: "create",
+        name: NEW_SESSION_NAME
+    }),
+    method: "PUT"
+};
+const maxRetries = 15; // Set the maximum number of retries
+
+fetchWithRetry(url, options, maxRetries);
